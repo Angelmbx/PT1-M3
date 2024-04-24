9480d7995f2041419b1ee734344bf720a2a04c62
Angelmbx <mirandabl.angel@gmail.com>
2024-04-25 01:13:30 +0200

    Initial commit.

---
+++ src/test/java/com/example/M3PruebaTecnica/M3PruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.M3PruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3PruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
