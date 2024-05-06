b45e1cca8b5ce95a9dbf28ea179d8b70dea0ea81
Angelmbx <mirandabl.angel@gmail.com>
2024-05-07 00:01:42 +0200

    application.yml & pom.xml updated

--- src/main/java/com/example/M3PruebaTecnica/controller/LenguageController.java
+++ src/main/java/com/example/M3PruebaTecnica/controller/LenguageController.java
@@ -1,2 +1,18 @@
-package com.example.M3PruebaTecnica.controller;public class LenguageController {
+package com.example.M3PruebaTecnica.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/lenguages")
+public class LenguageController {
+
+
+    @GetMapping
+    public String testController() {
+        return "Lenguages controller working, boss";
+    }
+
 }
