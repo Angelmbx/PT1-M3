c6274131f65ea1f4b3cde8467a54844e364bc81f
Angelmbx <mirandabl.angel@gmail.com>
2024-05-07 00:23:24 +0200

    Lenguages controller updated

--- src/main/java/com/example/M3PruebaTecnica/controller/LenguageController.java
+++ src/main/java/com/example/M3PruebaTecnica/controller/LenguageController.java
@@ -1,18 +1,42 @@
 package com.example.M3PruebaTecnica.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.M3PruebaTecnica.api.ILenguageService;
+import com.example.M3PruebaTecnica.model.dto.LenguageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/lenguages")
 public class LenguageController {
 
+    @Autowired
+    private ILenguageService lenguageService;
 
     @GetMapping
     public String testController() {
-        return "Lenguages controller working, boss";
+        return "Lenguages controller works, boss";
     }
 
+    @PostMapping
+    public String testController(@RequestBody String name) {
+        return "Lenguages controller works, " + name + "!";
+    }
+
+    @PostMapping(value = "/get")
+    public LenguageDTO queryLenguage(@RequestBody LenguageDTO lenguageDTO) { return lenguageService.queryLenguage(lenguageDTO); }
+
+    @GetMapping(value = "/getAll")
+    public List<LenguageDTO> queryAllLenguages() { return lenguageService.queryAllLenguages(); }
+
+    @PostMapping(value = "/add")
+    public int addLenguage(@RequestBody LenguageDTO lenguageDTO) { return lenguageService.insertLenguage(lenguageDTO);}
+
+    @PutMapping(value= "/update")
+    public int updateLenguage(@RequestBody LenguageDTO lenguageDTO) { return lenguageService.updateLenguage(lenguageDTO);}
+
+    @DeleteMapping(value = "/delete")
+    public int deleteLenguage(@RequestBody LenguageDTO lenguageDTO) { return lenguageService.deleteLenguage(lenguageDTO);}
 }
